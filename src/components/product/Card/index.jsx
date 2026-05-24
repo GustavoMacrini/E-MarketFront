878a8129210975b24cc133e5aad4bc50a1dbe0bc
Gustavo Macrini <82122229+GustavoMacrini@users.noreply.github.com>
2026-05-24 11:26:13 -0300

    separando front

---
+++ src/components/product/Card/index.jsx
@@ -0,0 +1,44 @@
+﻿import PcStyle from "./PcCard.module.css";
+import MobileStyle from "./MobileCard.module.css";
+import { FaHeart } from "react-icons/fa";
+import { FaCirclePlus } from "react-icons/fa6";
+
+//configurar css pc ou mobile
+let style = null;
+let usingPc = null;
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+function handleTabletChange(e) {
+    if (e.matches) {
+        style = PcStyle;
+        usingPc = true;
+    } else {
+        style = MobileStyle;
+        usingPc = false;
+    }
+}
+// Escutar mudanças
+mediaQuery.addEventListener('change', handleTabletChange);
+// Chamar a função imediatamente para configurar o estado inicial
+handleTabletChange(mediaQuery);
+
+function ProductCard({ openModal, productTitle, price, description }) {
+    let image = "https://d1pfez3jlrpp4u.cloudfront.net/imagem-ecommerce-barbosa/tiny-7bf39bb189f6abccfec278fd5053ac88-7896401100301.jpg";
+    
+    return (
+        <div className={style.wrapper}>            
+            <div className={style.heartIcon}>
+                <FaHeart color="lightgrey" size={usingPc ? "2em" : "1.5em"} />
+            </div>
+            <img className={style.image} draggable="false" src={image} onClick={() => openModal({ productTitle, price, image, description })}></img>                           
+            <div className={style.plusIconWrapper}>
+                <FaCirclePlus className={style.plusIcon} color="#0831A3" size={usingPc ? "2.5em" : "1.5em"} />
+            </div>
+            <div className={style.titleWrapepr}>
+                <h1 className={style.title} onClick={() => openModal({ productTitle, price, image, description })}>{productTitle}</h1>
+            </div>
+            <h2 className={style.value}>{"R$ " + price}</h2>
+        </div>
+    );
+}
+
+export default ProductCard;
---
+++ src/components/product/OverviewModal/index.jsx
@@ -0,0 +1,112 @@
+﻿
+import { useEffect, useRef, useState } from 'react';
+import PcStyle from './PcOverview.module.css';
+import MobileStyle from './MobileOverview.module.css';
+import { FaHeart } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+
+//configurar css pc ou mobile
+let style = null;
+let usingPc = null;
+const mediaQuery = window.matchMedia('(min-width: 768px)');
+function handleTabletChange(e) {
+    if (e.matches) {
+        style = PcStyle;
+        usingPc = true;
+    } else {
+        style = MobileStyle;
+        usingPc = false;
+    }
+}
+// Escutar mudanças
+mediaQuery.addEventListener('change', handleTabletChange);
+// Chamar a função imediatamente para configurar o estado inicial
+handleTabletChange(mediaQuery);
+
+
+function Description({ description }) {   
+    
+    const [open, setOpen] = useState(usingPc);
+
+    return (
+        <div className={style.descriptionWrapper}>
+            <div className={style.arrowWrapper }>
+                <h1 className={style.descriptionTitle} onClick={() => { setOpen(!open) }}>Informações adicionais</h1>
+                {open ? <IoIosArrowUp size="1.3em" className={style.arrow} onClick={() => { setOpen(!open) }} /> :
+                        <IoIosArrowDown size="1.3em" className={style.arrow} onClick={() => { setOpen(!open) }} />}
+                
+            </div>
+
+            <div className={open ? style.description : null}>{open ? description : null}</div>
+        </div>        
+    );
+}
+
+function ProductOverviewModal({ isOpen, closeModal, title, price, image, description }) {    
+    
+    // Hook para detectar cliques fora do modal
+    const useOutsideClick = (ref, callback) => {
+        useEffect(() => {
+            const handleClick = (e) => {
+                if (ref.current && !ref.current.contains(e.target)) {
+                    callback();
+                }
+            };
+            document.addEventListener('mousedown', handleClick);
+            return () => document.removeEventListener('mousedown', handleClick);
+        }, [ref, callback]);
+    };
+
+    const modalRef = useRef(null);
+    useOutsideClick(modalRef, closeModal);
+
+    if (isOpen != true) {
+        return null;
+    }
+    
+    return (
+        <div className={style.background}> 
+            <div className={style.modal}>
+                <div ref={modalRef} className={style.wrapper}>
+                    <div className={style.product}>                            
+                        <div className={style.closeButtonWrapper}>
+                            <div className={style.closeButton}>
+                                <IoMdClose size="2em" className={style.closeButtonIcon} onClick={() => closeModal()} />
+                            </div>
+
+                            <div className={style.productWrapper}>
+                                <div className={style.imageWrapper}>
+                                    <img className={style.image} src={image}></img>
+                                </div>
+
+                                <div className={style.detailDescriptionWrapper}>
+                                    <div className={style.detailWrapper}>
+
+                                        <div className={style.detail}>
+
+                                            <div className={style.titleWrapper}>
+                                                <h1 className={style.title}>{title}</h1>
+                                                <FaHeart color="lightgrey" size="2em" className={style.heartIcon} />
+                                            </div>
+
+                                            <h2 className={style.value}>{"R$ " + price}</h2>                                            
+                                            <button className={style.btn}>Comprar</button>
+                                            
+                                        </div>
+                                    </div>
+                                    <Description description={description} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+    
+    
+}
+
+export default ProductOverviewModal;
